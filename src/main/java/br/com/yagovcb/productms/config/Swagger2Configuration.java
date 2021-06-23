e25a9db9ae7537e521fb1d0e915d60cfb3644ff0
Yago do Valle Castelo Branco <yago.valle@itspay.com.br>
2021-06-23 19:15:41 -0300

    Ajustes na configuração do Swagger

--- src/main/java/br/com/yagovcb/productms/config/Swagger2Configuration.java
+++ src/main/java/br/com/yagovcb/productms/config/Swagger2Configuration.java
@@ -4,9 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 /**
  *  Classe de Configuração do Swagger
@@ -25,6 +29,17 @@ public class Swagger2Configuration {
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("br.com.yagovcb.productms.controller"))
                 .paths(PathSelectors.ant("/**"))
-                .build();
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Gerenciamento de produtos API",
+                "API para gerenciamento de produtos",
+                "API Versão 1",
+                "Terms of service",
+                new Contact("Yago Castelo Branco", "https://github.com/Yagovcb/product-ms", "yago.vcb@gmail.com"),
+                "License of API", "API license URL", Collections.emptyList());
     }
 }
